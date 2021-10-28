7f257189e4d2c416560052fa48a9629aaf9d3ff9
gauravkatwate <89981318+gauravkatwate@users.noreply.github.com>
2021-10-28 23:41:27 +0530

    Add files via upload

---
+++ Loop Programms/B.5.Print Table Inputed Number.c
@@ -0,0 +1,17 @@
+#include<conio.h>
+#include<stdio.h>
+
+int main()
+{
+    int ino = 0,i = 0;
+
+    printf("\n Enter Any Number To Get Table => ");
+    scanf("%d",&ino);
+
+    for(i = 1; i <= 10; i++ )
+    {
+        printf("\n%3d * %3d => %3d",ino,i,ino * i);
+    }
+    getch();
+    return 0;
+}
