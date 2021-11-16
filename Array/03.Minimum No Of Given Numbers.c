cf7218f312a35ccd474f370535afc07b9f6702ff
Gaurav <gauravKatwate7891@gmail.com>
2021-11-17 01:04:04 +0530

    Array Programms

---
+++ Array/03.Minimum No Of Given Numbers.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   static int i = 0, min = 0;
+    int ArrMin[] = {0}, cnt = 0;;
+
+    printf("\n Enter Count Of Numbers = >");
+    scanf("%d",&cnt);
+
+    for(i = 0; i < cnt ; i++)
+    {
+        printf("\n Enter %d No => ",(i + 1));
+        scanf("%d",&ArrMin[i]);
+
+        if(i == 0 || ArrMin[i] < min)
+        {
+            min = ArrMin[i];
+        }
+    }
+
+    printf("\n Minimum Number Of Given Numbers Is %d ",min);
+
+    getch();
+    return 0;
+}
