fac612c9d020a95ac8e5d6b2b5be09dc5d7e4955
Gaurav <gauravKatwate7891@gmail.com>
2021-11-18 01:33:16 +0530

    Array Programms

---
+++ Array/05.Create Array Of 7 Elements With Null initialize & Display All.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Arr[7] = {};
+
+    printf("\n Array 1st Element => %d",Arr[0]);
+    printf("\n Array 2nd Element => %d",Arr[1]);
+    printf("\n Array 3rd Element => %d",Arr[2]);
+    printf("\n Array 4th Element => %d",Arr[3]);
+    printf("\n Array 5th Element => %d",Arr[4]);
+    printf("\n Array 6th Element => %d",Arr[5]);
+    printf("\n Array 7th Element => %d",Arr[6]);
+
+    getch();
+    return 0;
+}
