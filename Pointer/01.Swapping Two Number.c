5dfd43ba5bd122d5c9d0ec670fb4a614c9c77c6a
Gaurav <gauravKatwate7891@gmail.com>
2021-12-06 22:47:22 +0530

    c Pointers

---
+++ Pointer/01.Swapping Two Number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+void swap(int* no1,int* no2)
+{
+    *no1 = *no1 + *no2;
+    *no2 = *no1 - *no2;
+    *no1 = *no1 - *no2;
+}
+
+int main()
+{
+    int ino1 = 0, ino2 = 0;
+
+    printf("\n Enter First Number =>");
+    scanf("%d",&ino1);
+
+    printf("\n Enter Second Number =>");
+    scanf("%d",&ino2);
+
+    swap(&ino1,&ino2);
+
+    printf("\n After Swapping First Number => %d",ino1);
+    printf("\n After Swapping Second Number => %d",ino2);
+
+    getch();
+    return 0;
+}
